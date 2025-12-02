edad9aefe8c73898753a8eb04ecf9761b3035f43
cxrala <ameliang123@gmail.com>
2025-12-02 09:18:29 +0000

    oops i accidentally broke part 1 on the way to solving part 2

--- lib/day2.ml
+++ lib/day2.ml
@@ -4,6 +4,16 @@ open Helpers.Mylist
 (* constants *)
 let lines = Helpers.Lines.read_lines "inputs/day2.txt"
 
+(* helpers *)
+
+let parse_input line =
+  let parse_range segment =
+    match String.split_on_char '-' segment with
+    | [ a; b ] -> (int_of_string a, int_of_string b)
+    | _ -> invalid_arg "invalid segment"
+  in
+  String.split_on_char ',' line |> List.map parse_range
+
 (* brute force *)
 
 let is_invalid_id x substr_length =
@@ -13,7 +23,7 @@ let is_invalid_id x substr_length =
 
 let is_invalid_id_pt1 x =
   let string_length = String.length @@ string_of_int x in
-  string_length % 2 = 0 && is_invalid_id (string_length / 2) x
+  string_length % 2 = 0 && is_invalid_id x (string_length / 2)
 
 let is_invalid_id_pt2 x =
   let max_substr_length = (String.length @@ string_of_int x) / 2 in
@@ -23,21 +33,13 @@ let is_invalid_id_pt2 x =
 
 let rec invalid_ids start_range end_range =
   match start_range with
-  | hd when start_range <= end_range && is_invalid_id_pt2 hd ->
+  | hd when start_range <= end_range && is_invalid_id_pt1 hd ->
       hd :: invalid_ids (start_range + 1) end_range
   | _ when start_range > end_range -> []
   | _ -> invalid_ids (start_range + 1) end_range
 
 let sum_invalid_ids (start_range, end_range) =
   invalid_ids start_range end_range |> List.fold_left ( + ) 0
-
-let parse_input line =
-  let parse_range segment =
-    match String.split_on_char '-' segment with
-    | [ a; b ] -> (int_of_string a, int_of_string b)
-    | _ -> invalid_arg "invalid segment"
-  in
-  String.split_on_char ',' line |> List.map parse_range
 
 let sol_1 =
   List.fold_left ( + ) 0
